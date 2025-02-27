fbf2173e29acb573c6de3daa866485b580dc1364
p3dro3ssa <pedro.krzyszczak@gmail.com>
2025-02-27 16:53:38 +0100

    point added

--- svg/src/Point.java
+++ svg/src/Point.java
@@ -1,2 +1,24 @@
 public class Point {
+    public double x;
+    public double y;
+    @Override
+    public String toString(){
+        return "("+x+" , "+y+")";
+    }
+    public String toSvg()
+    {
+        return "<circle r=\"5\" cx=\""+x+"\" cy=\""+y+"\" fill=\"black\" />";
+    }
+    public void translate(double dx, double dy)
+    {
+        x+=dx;
+        this.y+=dy;
+    }
+    public Point translated(double dx, double dy)
+    {
+        Point newPoint = new Point();
+        newPoint.x = x + dx;
+        newPoint.y = y + dy;
+        return newPoint;
+    }
 }
