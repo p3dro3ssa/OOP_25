797b26609bf0b47cacd3e3f793a1461539b32d17
p3dro3ssa <pedro.krzyszczak@gmail.com>
2025-03-06 15:50:36 +0100

    .

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -2,9 +2,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
-        Point p1 = new Point(x:30 , y:40);
-        Pint p2 = new Point();
-        p2.setY(40);
+        Point p1 = new Point(getX(30) , getY(40));
+        Point p2 = new Point(getX(40) , getY());
+
         System.out.println(p1);
         System.out.println(p2);
 
