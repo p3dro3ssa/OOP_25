7c524796c2fde37c14febfd3305b9a90e046be94
p3dro3ssa <pedro.krzyszczak@gmail.com>
2025-03-06 16:12:51 +0100

    all

--- svg/src/Segment.java
+++ svg/src/Segment.java
@@ -1,22 +1,36 @@
 import static java.lang.Math.pow;
 
 public class Segment {
-    public Point a, b;
+    private final Point a;
+    private final Point b;
+
+    public Segment(Point a, Point b) {
+        this.a = new Point(a);      // wykorzystujemy konstrukor kopiujący
+        this.b = new Point(b);
+    }
 
     public double length(){
         return Math.sqrt(pow(a.getX()-b.getX(), 2) + pow(a.getY()-b.getY(), 2));
-
     }
 
     public static Segment maxSegment(Segment[] arr){
-        if(arr.length == 0)
+        if (arr.length == 0)
             return null;
 
         Segment max = arr[0];
-        for(int i=1; i<arr.length; i++){
-            if(arr[i].length() > max.length())
+        for(int i=1; i < arr.length; i++){
+            if(arr[i].length() > max.length()){
                 max = arr[i];
+            }
         }
         return max;
+    }
+
+    @Override
+    public String toString() {
+        return "Segment{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
     }
 }
