2a4922b2ae1fe49eae7e7dbd49466da5b2042c70
p3dro3ssa <pedro.krzyszczak@gmail.com>
2025-03-06 16:12:14 +0100

    all

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,12 +1,17 @@
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        Point p1 = new Point(getX(30) , getY(40));
-        Point p2 = new Point(getX(40) , getY());
-
+        Point p1 = new Point(30 ,40);
+        Point p2 = new Point();
+        p2.setY(40);
         System.out.println(p1);
         System.out.println(p2);
+
+        Segment seg = new Segment(p1, p2);
+        System.out.println(seg);
+        p2.setX(60);
+        System.out.println(seg);
+
+    }
 
 
 }
